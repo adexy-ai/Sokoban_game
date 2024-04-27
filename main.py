a551801f1488c3542ec0fc8f93fb4344df7374d1
Anuoluwapo ADENIRAN <69057307+adexy-ai@users.noreply.github.com>
2024-04-27 12:49:24 +0300

    Create main.py
    Created the python file and wrote down a few lines of comment on how to proceed with the project

---
+++ main.py
@@ -0,0 +1,5 @@
+# Follow the instruction for creating the game on the course page
+# Create a folder to copy the needed accessories for the game e.g robot, shape etc.
+# Have a sketch and outline of the programming logic and steps 
+# Comment the code throughout the game building process for readability and ease of review
+
